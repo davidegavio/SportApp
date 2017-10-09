05fce188e9279ee33ca8f74685a2a76afdb77ea3
Davide Gavio <20010562@studenti.uniupo.it>
2017-10-09 20:45:46 +0200

    Prima inizializzazione.
    Creato un tema dedicato, creata un'icona dedicata. Inizializzato una prima versione del menù.

---
+++ app/src/main/java/it/uniupo/sportapp/fragments/SportChooserFragment.java
@@ -0,0 +1,22 @@
+package it.uniupo.sportapp.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dgavio on 09/10/17.
+ */
+
+public class SportChooserFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //TODO complete fregment
+        // Inflate the layout for this fragment
+        return null;
+    }
+}
